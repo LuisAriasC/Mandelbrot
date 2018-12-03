131af2a7dd64b2b2da0b98418108cb1868221eca
LuisAriasC <luis.ac945@gmail.com>
2018-12-03 00:02:11 -0600

    sc

--- mandelbrotKernel.h
+++ mandelbrotKernel.h
@@ -1,4 +1,5 @@
 #include "lines.h"
+#include "common.h"
 #include "Mandelbrot.h"
 
 using namespace std;
@@ -15,5 +16,11 @@ void runCuda(std::unique_ptr<int [], std::default_delete<int []>>& m_fractal,std
   int fractalBytes = M_WIDTH * M_HEIGHT * sizeof(int);
   int histogramBytes = Mandelbrot::MAX_ITERATIONS * sizeof(int);
 
+  d_fractal = (int *)malloc(fractalBytes);
+  d_histogram = (int *)malloc(histogramBytes);
+  memset(d_fractal, 0, fractalBytes);
+  memset(d_histogram, 0, histogramBytes);
 
+  SAFE_CALL(cudaMalloc<int>(&d_fractal, fractalBytes), "CUDA Malloc Failed");
+  SAFE_CALL(cudaMalloc<int>(&d_histogram, histogramBytes), "CUDA Malloc Failed");
 }
