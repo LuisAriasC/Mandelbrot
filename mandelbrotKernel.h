64fc96743f0b33a5b4dba8b6379d2c4421ff39c5
LuisAriasC <luis.ac945@gmail.com>
2018-12-03 00:24:41 -0600

    sc

--- mandelbrotKernel.h
+++ mandelbrotKernel.h
@@ -11,7 +11,7 @@ __global__ void kernel(){
 
 void runCuda(std::unique_ptr<int [], std::default_delete<int []>>& m_fractal,std::unique_ptr<int [], std::default_delete<int []>>& m_histogram, double scale, double xCenter, double yCenter){
 
-  int* d_fractal, d_histogram;
+  int * d_fractal, * d_histogram;
   int fractalBytes = M_WIDTH * M_HEIGHT * sizeof(int);
   int histogramBytes = Mandelbrot::MAX_ITERATIONS * sizeof(int);
 
@@ -19,5 +19,5 @@ void runCuda(std::unique_ptr<int [], std::default_delete<int []>>& m_fractal,std
   d_histogram = (int *)malloc(histogramBytes);
   memset(d_fractal, 0, fractalBytes);
   memset(d_histogram, 0, histogramBytes);
-  
+
 }
