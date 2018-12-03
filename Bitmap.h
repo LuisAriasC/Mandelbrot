789dd4f3937ed0d9c7ca993bb6ba3c53060dab0f
LuisAriasC <luis.ac945@gmail.com>
2018-12-02 23:40:07 -0600

    sc

--- Bitmap.h
+++ Bitmap.h
@@ -6,13 +6,35 @@
 #include <memory>
 #include <fstream>
 //#include "Bitmap.h"
-#include "BitmapInfoHeader.h"
-#include "BitmapFileHeader.h"
+//#include "BitmapInfoHeader.h"
+//#include "BitmapFileHeader.h"
 
-using namespace caveofprogramming;
 using namespace std;
 
+#pragma pack(2)
+
 namespace caveofprogramming {
+
+struct BitmapFileHeader {
+	char header[2] { 'B', 'M' };
+	int32_t fileSize;
+	int32_t reserved { 0 };
+	int32_t dataOffset;
+};
+
+struct BitmapInfoHeader {
+	int32_t headerSize{40};
+	int32_t width;
+	int32_t height;
+	int16_t planes{1};
+	int16_t bitsPerPixel{24};
+	int32_t compression{0};
+	int32_t dataSize{0};
+	int32_t horizontalResolution{2400};
+	int32_t verticalResolution{2400};
+	int32_t colors{0};
+	int32_t importantColors{0};
+};
 
 class Bitmap {
 private:
