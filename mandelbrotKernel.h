39ae577bbe35f15e0b80e12da6f3bb06cf7e2127
LuisAriasC <luis.ac945@gmail.com>
2018-12-03 00:27:52 -0600

    sc

--- mandelbrotKernel.h
+++ mandelbrotKernel.h
@@ -20,4 +20,11 @@ void runCuda(int * m_fractal,int * m_histogram, double scale, double xCenter, do
   memset(d_fractal, 0, fractalBytes);
   memset(d_histogram, 0, histogramBytes);
 
+
+  SAFE_CALL(cudaMalloc<int>(&d_histogram, histogramBytes), "CUDA Malloc Failed");
+  SAFE_CALL(cudaMalloc<int>(&d_fractal, fractalBytes), "CUDA Malloc Failed");
+
+  SAFE_CALL(cudaMemcpy(d_histogram, m_histogram, histogramBytes, cudaMemcpyHostToDevice), "CUDA Memcpy Host To Device Failed");
+
+
 }
