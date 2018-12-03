5bacb0c6b2cbc8bf7cc25d76d064e3e24abf51e6
LuisAriasC <luis.ac945@gmail.com>
2018-12-03 01:42:28 -0600

    sc

--- mandelbrotKernel.h
+++ mandelbrotKernel.h
@@ -38,7 +38,7 @@ __global__ void kernel(int * d_fractal, int * d_histogram, int step,double scale
     cuDoubleComplex c = make_cuDoubleComplex(x, y);
 
     while(iterations < Mandelbrot::MAX_ITERATIONS) {
-      z = cuCmul(z, z) + c;
+      z = cuCadd(cuCmul(z, z), c);
 
       if(abs(z) > 2) {
         break;
